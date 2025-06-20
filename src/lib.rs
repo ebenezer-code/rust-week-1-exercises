efa6f20822f7a62dc67ab20ab1ff92d0d4806ded
Ebenezer Noble <ebenezernoblest@gmail.com>
2025-06-20 15:12:28 +0100

    Code refractor

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    // 🧪 Test: Short input
     if raw_tx_hex.len() < 8 {
-        return Err("Transaction data too short".to_string());
+        return Err("Transaction data is too short".to_string());
     }
 
-    let version_hex = &raw_tx_hex[..8];
+    let hex_version = &raw_tx_hex[..8];
 
-    // 🧪 Test: Invalid hex
     let bytes = (0..8)
         .step_by(2)
-        .map(|i| u8::from_str_radix(&version_hex[i..i + 2], 16))
+        .map(|i| u8::from_str_radix(&hex_version[i..i + 2], 16))
         .collect::<Result<Vec<u8>, _>>()
         .map_err(|_| "Hex decode error".to_string())?;
 
