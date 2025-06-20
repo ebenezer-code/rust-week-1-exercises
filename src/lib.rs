9d5096309baa6ebd5756fcf4809cf6c54d3e4204
Ebenezer Noble <ebenezernoblest@gmail.com>
2025-06-20 15:11:03 +0100

    passed all test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-// Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    // 🧪 Test: Short input
+    if raw_tx_hex.len() < 8 {
+        return Err("Transaction data too short".to_string());
+    }
+
+    let version_hex = &raw_tx_hex[..8];
+
+    // 🧪 Test: Invalid hex
+    let bytes = (0..8)
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&version_hex[i..i + 2], 16))
+        .collect::<Result<Vec<u8>, _>>()
+        .map_err(|_| "Hex decode error".to_string())?;
+
+    if bytes.len() != 4 {
+        return Err("Invalid version length".to_string());
+    }
+
+    let version = u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
+    Ok(version)
 }
